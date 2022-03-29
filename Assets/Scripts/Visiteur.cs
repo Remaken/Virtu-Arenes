6650a894be1d41767b85fb18f7e6704d3bcbe81f
Remaken <jorgeazevedo24@gmail.com>
2022-03-29 16:50:37 +0200

    Alpha0.03(Mouvement)

--- Assets/Scripts/Visiteur.cs
+++ Assets/Scripts/Visiteur.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Visiteur : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed = 5f;
+    private void FixedUpdate()
     {
-        
+        PlayerMouvement();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlayerMouvement()
     {
         
+        this.transform.Translate(0f,0f,Input.GetAxis("Vertical")*_moveSpeed * Time.fixedDeltaTime);
+        this.transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal")*_moveSpeed*10f*Time.fixedDeltaTime, 0f));
     }
 }
