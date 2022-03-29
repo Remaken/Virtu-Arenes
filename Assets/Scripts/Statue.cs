1be11d3604e5577abb45bfcfd955d56faa0d9dba
Remaken <jorgeazevedo24@gmail.com>
2022-03-29 18:27:13 +0200

    Alpha0.6(Deplacement des Objets)

--- Assets/Scripts/Statue.cs
+++ Assets/Scripts/Statue.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 
 public class Statue : MonoBehaviour
 {
-    public GameObject[] emplacements;
+    public GameObject[] equipements;
     private bool _epeeDetruite = false;
     private bool _bouclierDetruit = false;
     private bool _casqueDetruit = false;
     private bool _plastronDetruit = false;
 
+    
+    //  NOTE :  Element 0 = Plastron  | Element 1 = Casque  | Element 2 = Epee  |  Element 3 = Bouclier  des deux Tableaux
     private void OnEnable()
     {
         Bouclier.ShieldWasTaken += BouclierDetruit;
@@ -19,21 +21,44 @@ public class Statue : MonoBehaviour
         Plastron.BreastPlateWasTaken += PlastronDetruit;
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlacementBouclier();
+            PlacementCasque();
+            PlacementEpee();
+            PlacementPlastron();
+        }
+    }
+
     private void PlacementBouclier()
     {
-        
+        if (_bouclierDetruit==true)
+        {
+            equipements[3].SetActive(true);
+        }
     }
     private void PlacementEpee()
     {
-        
+        if (_epeeDetruite==true)
+        {
+            equipements[2].SetActive(true);
+        }
     }
     private void PlacementPlastron()
     {
-        
+        if (_plastronDetruit==true)
+        {
+            equipements[0].SetActive(true);
+        }
     }
     private void PlacementCasque()
     {
-        
+        if (_casqueDetruit==true)
+        {
+            equipements[1].SetActive(true);
+        }
     }
     
     private void BouclierDetruit()
