25429f6056ebe3d0e6073abc4641e1b77f6d2b50
Djey013 <97234391+Djey013@users.noreply.github.com>
2022-05-20 17:17:42 +0200

    Update Lever_Mecanism.cs

--- Assets/Dossier Persos/Jeremy/Scripts/Lever_Mecanism.cs
+++ Assets/Dossier Persos/Jeremy/Scripts/Lever_Mecanism.cs
@@ -2,17 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Lever_Mecanism : MonoBehaviour
 {
+    public XRGrabInteractable grab;
     public GameObject sword;
+    public Vector3 swordSpaceSettings = new Vector3(0f, 0f, 0f);
+
+
+    private void OnEnable()
+    {
+        grab.selectEntered.AddListener(SwordPosition);
+        
+    }
+
+    private void OnDisable()
+    {
+        grab.selectEntered.AddListener(SwordPosition);
+        
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Poignee"))
         {
             sword.SetActive(true);
-            
+            //SwordPosition?.Invoke();
+
         }
+    }
+
+    public void SwordPosition(SelectEnterEventArgs args)
+    {
+        sword.transform.position = sword.transform.position + swordSpaceSettings;
     }
     
     
