7609e67c673f186f345101a7cb5ca3d4c9d0964f
Djey013 <jeremy.vernagallo@hotmail.fr>
2022-05-22 19:59:31 +0200

    MAJ

--- Assets/Dossier Persos/Jeremy/Scripts/Object_Interaction.cs
+++ Assets/Dossier Persos/Jeremy/Scripts/Object_Interaction.cs
@@ -1,36 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Object_Interaction : MonoBehaviour
 {
     [SerializeField] XRGrabInteractable grab;
-    public GameObject sword;
+    
+    public Transform swordPosition;
     public Vector3 swordSpaceSettings = new Vector3(0f, 0f, 0f);
+    
+    public bool action = true;
+    public bool isSelected = true;
+   
+    
     
     private void OnEnable()
     {
-        grab.selectEntered.AddListener(SwordPosition);
+        grab.selectEntered.AddListener(StartSelect);
+        grab.selectExited.AddListener(StopSelect);
         
     }
 
     private void OnDisable()
     {
-        grab.selectEntered.AddListener(SwordPosition);
-        
+        grab.selectEntered.RemoveListener(StartSelect);
+        grab.selectExited.RemoveListener(StopSelect);
     }
-    
+
+    private void Start()
+    {
+        action = false;
+        isSelected = false;
+
+    }
+
     void Update()
     {
         //SwordPosition(SelectEnterEventArgs args);
+        
+       
     }
     
-    public void SwordPosition(SelectEnterEventArgs args)
+    public void StartSelect(SelectEnterEventArgs args)
     {
-        sword.transform.position = sword.transform.position + swordSpaceSettings;
+        
+        action = true;
+        
+        if (action)
+        {
+            args.interactableObject.transform.position = args.interactorObject.transform.position + swordSpaceSettings;
+            
+            isSelected = true;
+        }
+        
+
+    }
+    
+    public void StopSelect(SelectExitEventArgs args)
+    {
+        action = false;
+        
+        if (!action)
+        {
+            isSelected = false;
+            
+            //gameObject.GetComponent<Rigidbody>().isKinematic = false;   //drop the sword with gravity enabled
+            //gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+        
+
+        
         
     }
-    
     
 }
